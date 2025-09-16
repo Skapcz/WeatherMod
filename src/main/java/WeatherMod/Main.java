f4300fe85a6b620a096978f74c1e9882ba3f3ab9
Skapcz <nitram.t10@gmail.com>
2025-09-16 17:06:18 +0200

    Version 0.1-1

---
+++ src/main/java/WeatherMod/Main.java
@@ -0,0 +1,17 @@
+package WeatherMod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Main.MODID)
+public class Main {
+    public static final String MODID = "weather_mod";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
