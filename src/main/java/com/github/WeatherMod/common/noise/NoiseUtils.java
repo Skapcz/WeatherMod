31b325122ea2cbbe3d7dbe1b066651ac75fb344f
Skapcz <nitram.t10@gmail.com>
2025-10-17 13:56:39 +0200

    Version 0.1-6.7 -visualization -warp

--- src/main/java/com/github/WeatherMod/common/noise/NoiseUtils.java
+++ src/main/java/com/github/WeatherMod/common/noise/NoiseUtils.java
@@ -2,7 +2,10 @@ package com.github.WeatherMod.common.noise;
 
 public class NoiseUtils {
 
-    public static double fractalNoise (OpenSimplexNoise noise, int layers, double x, double y, double lacunarity, double gain){
+    public static double fractalNoise (OpenSimplexNoise noise, int layers, double x, double y, double lacunarity, double gain, double warpStrength, double warpScale){
+        double warpX = noise.eval(x * warpScale, y * warpScale) * warpStrength;
+        double warpY = noise.eval(x * warpScale + 100, y * warpScale + 100) * warpStrength;
+
         double result = 0;
         double frequency = 1;
         double maxAmplitude = 0;
@@ -10,11 +13,13 @@ public class NoiseUtils {
 
 
         for (int i = 0; i < layers; i++){
+
             double offsetX = i * 37.17;
             double offsetY = i * 91.83;
-            result += noise.eval((x + offsetX) * frequency, (y + offsetY) * frequency) * amplitude;
+            result += noise.eval((x + warpX + offsetX) * frequency, (y + warpY + offsetY) * frequency) * amplitude;
             frequency *= lacunarity;
             amplitude *= gain;
+            maxAmplitude += amplitude;
 
         }
         return result / maxAmplitude;
